baec8b0cf475a4115147c5cf155d921cd866974c
Kota Nonaka <kota.kaicho@gmail.com>
2022-12-04 17:58:10 +0900

    loading

---
+++ app/(default)/p/[id]/loading.tsx
@@ -0,0 +1,11 @@
+const ArticleLoading = () => {
+  return (
+    <div className={'flex gap-2 justify-center py-8'}>
+      <div className={'w-3 h-3 rounded-full bg-gray-400 animate-bounce'} />
+      <div className={'w-3 h-3 rounded-full bg-gray-400 animate-bounce'} style={{animationDelay: '300ms'}} />
+      <div className={'w-3 h-3 rounded-full bg-gray-400 animate-bounce'} style={{animationDelay: '600ms'}} />
+    </div>
+  )
+}
+
+export default ArticleLoading
