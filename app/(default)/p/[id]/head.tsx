bb76b7754ebd3d272a25daf84a7889da4048533b
Kota Nonaka <kota.kaicho@gmail.com>
2022-12-04 19:28:58 +0900

    modify head

--- app/(default)/p/[id]/head.tsx
+++ app/(default)/p/[id]/head.tsx
@@ -1,10 +1,12 @@
 import {getClient, getPageMeta} from '../../../../api/notion'
+import DefaultHead from '../../../../components/defaultHead'
 
 const ArticlePageHead = async ({params: {id}}: {params: {id: string}}) => {
   const {title: articleTitle, description} = await getPageMeta(getClient(), id)
 
   return (
     <>
+      <DefaultHead />
       <title>{`${articleTitle} - tips chips`}</title>
       <meta property={'og:title'} content={articleTitle} />
       <meta property={'og:description'} content={description} />
---
+++ components/defaultHead.tsx
@@ -0,0 +1,7 @@
+const DefaultHead = () => {
+  return (
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+  )
+}
+
+export default DefaultHead
